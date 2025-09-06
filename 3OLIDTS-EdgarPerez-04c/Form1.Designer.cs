0fe318d768387f8b15d82cba08845ba89813a413
Edgar Pérez <edgarperezperez554@gmail.com>
2025-09-06 12:47:35 -0600

    Agregar archivos de proyecto.

---
+++ 3OLIDTS-EdgarPerez-04c/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace _3OLIDTS_EdgarPerez_04c
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbNombre = new System.Windows.Forms.Label();
+            this.lbApellido = new System.Windows.Forms.Label();
+            this.lbTelefono = new System.Windows.Forms.Label();
+            this.lbEstatura = new System.Windows.Forms.Label();
+            this.lbEdad = new System.Windows.Forms.Label();
+            this.tbApellido = new System.Windows.Forms.TextBox();
+            this.tbTelefono = new System.Windows.Forms.TextBox();
+            this.tbEstatura = new System.Windows.Forms.TextBox();
+            this.tbEdad = new System.Windows.Forms.TextBox();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.gbGenero = new System.Windows.Forms.GroupBox();
+            this.rbFemenino = new System.Windows.Forms.RadioButton();
+            this.rbMasculino = new System.Windows.Forms.RadioButton();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.gbGenero.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbNombre
+            // 
+            this.lbNombre.AutoSize = true;
+            this.lbNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNombre.Location = new System.Drawing.Point(70, 37);
+            this.lbNombre.Name = "lbNombre";
+            this.lbNombre.Size = new System.Drawing.Size(66, 16);
+            this.lbNombre.TabIndex = 0;
+            this.lbNombre.Text = "Nombre:";
+            // 
+            // lbApellido
+            // 
+            this.lbApellido.AutoSize = true;
+            this.lbApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbApellido.Location = new System.Drawing.Point(70, 81);
+            this.lbApellido.Name = "lbApellido";
+            this.lbApellido.Size = new System.Drawing.Size(69, 16);
+            this.lbApellido.TabIndex = 1;
+            this.lbApellido.Text = "Apellido:";
+            // 
+            // lbTelefono
+            // 
+            this.lbTelefono.AutoSize = true;
+            this.lbTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTelefono.Location = new System.Drawing.Point(70, 123);
+            this.lbTelefono.Name = "lbTelefono";
+            this.lbTelefono.Size = new System.Drawing.Size(73, 16);
+            this.lbTelefono.TabIndex = 2;
+            this.lbTelefono.Text = "Telefono:";
+            // 
+            // lbEstatura
+            // 
+            this.lbEstatura.AutoSize = true;
+            this.lbEstatura.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEstatura.Location = new System.Drawing.Point(70, 169);
+            this.lbEstatura.Name = "lbEstatura";
+            this.lbEstatura.Size = new System.Drawing.Size(68, 16);
+            this.lbEstatura.TabIndex = 3;
+            this.lbEstatura.Text = "Estatura:";
+            // 
+            // lbEdad
+            // 
+            this.lbEdad.AutoSize = true;
+            this.lbEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEdad.Location = new System.Drawing.Point(86, 215);
+            this.lbEdad.Name = "lbEdad";
+            this.lbEdad.Size = new System.Drawing.Size(48, 16);
+            this.lbEdad.TabIndex = 4;
+            this.lbEdad.Text = "Edad:";
+            // 
+            // tbApellido
+            // 
+            this.tbApellido.Location = new System.Drawing.Point(147, 75);
+            this.tbApellido.Name = "tbApellido";
+            this.tbApellido.Size = new System.Drawing.Size(147, 22);
+            this.tbApellido.TabIndex = 5;
+            // 
+            // tbTelefono
+            // 
+            this.tbTelefono.Location = new System.Drawing.Point(147, 117);
+            this.tbTelefono.Name = "tbTelefono";
+            this.tbTelefono.Size = new System.Drawing.Size(147, 22);
+            this.tbTelefono.TabIndex = 6;
+            // 
+            // tbEstatura
+            // 
+            this.tbEstatura.Location = new System.Drawing.Point(147, 163);
+            this.tbEstatura.Name = "tbEstatura";
+            this.tbEstatura.Size = new System.Drawing.Size(147, 22);
+            this.tbEstatura.TabIndex = 7;
+            // 
+            // tbEdad
+            // 
+            this.tbEdad.Location = new System.Drawing.Point(147, 209);
+            this.tbEdad.Name = "tbEdad";
+            this.tbEdad.Size = new System.Drawing.Size(147, 22);
+            this.tbEdad.TabIndex = 8;
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(147, 31);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(147, 22);
+            this.tbNombre.TabIndex = 9;
+            // 
+            // gbGenero
+            // 
+            this.gbGenero.Controls.Add(this.rbFemenino);
+            this.gbGenero.Controls.Add(this.rbMasculino);
+            this.gbGenero.Location = new System.Drawing.Point(41, 256);
+            this.gbGenero.Name = "gbGenero";
+            this.gbGenero.Size = new System.Drawing.Size(307, 156);
+            this.gbGenero.TabIndex = 10;
+            this.gbGenero.TabStop = false;
+            this.gbGenero.Text = "Genero";
+            // 
+            // rbFemenino
+            // 
+            this.rbFemenino.AutoSize = true;
+            this.rbFemenino.Location = new System.Drawing.Point(164, 40);
+            this.rbFemenino.Name = "rbFemenino";
+            this.rbFemenino.Size = new System.Drawing.Size(88, 20);
+            this.rbFemenino.TabIndex = 1;
+            this.rbFemenino.TabStop = true;
+            this.rbFemenino.Text = "Femenino";
+            this.rbFemenino.UseVisualStyleBackColor = true;
+            // 
+            // rbMasculino
+            // 
+            this.rbMasculino.AutoSize = true;
+            this.rbMasculino.Location = new System.Drawing.Point(6, 40);
+            this.rbMasculino.Name = "rbMasculino";
+            this.rbMasculino.Size = new System.Drawing.Size(89, 20);
+            this.rbMasculino.TabIndex = 0;
+            this.rbMasculino.TabStop = true;
+            this.rbMasculino.Text = "Masculino";
+            this.rbMasculino.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(68, 434);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(112, 62);
+            this.btnGuardar.TabIndex = 11;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.Red;
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(205, 434);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(113, 62);
+            this.btnCancelar.TabIndex = 12;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 553);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.gbGenero);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.tbEdad);
+            this.Controls.Add(this.tbEstatura);
+            this.Controls.Add(this.tbTelefono);
+            this.Controls.Add(this.tbApellido);
+            this.Controls.Add(this.lbEdad);
+            this.Controls.Add(this.lbEstatura);
+            this.Controls.Add(this.lbTelefono);
+            this.Controls.Add(this.lbApellido);
+            this.Controls.Add(this.lbNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gbGenero.ResumeLayout(false);
+            this.gbGenero.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbNombre;
+        private System.Windows.Forms.Label lbApellido;
+        private System.Windows.Forms.Label lbTelefono;
+        private System.Windows.Forms.Label lbEstatura;
+        private System.Windows.Forms.Label lbEdad;
+        private System.Windows.Forms.TextBox tbApellido;
+        private System.Windows.Forms.TextBox tbTelefono;
+        private System.Windows.Forms.TextBox tbEstatura;
+        private System.Windows.Forms.TextBox tbEdad;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.GroupBox gbGenero;
+        private System.Windows.Forms.RadioButton rbFemenino;
+        private System.Windows.Forms.RadioButton rbMasculino;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
+
---
+++ 3OLIDTS-EdgarPerez-04c/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO; //Libreria para escritura y lectrura de archivos
+
+namespace _3OLIDTS_EdgarPerez_04c
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            tbApellido.Clear();
+            tbNombre.Clear();
+            tbTelefono.Clear();
+            tbEstatura.Clear();
+            tbEdad.Clear();
+            rbMasculino.Checked = false;
+            rbMasculino.Checked = false;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string apellido = tbApellido.Text;
+            string nombre = tbNombre.Text;
+            string telefono = tbTelefono.Text;
+            string estatura = tbEstatura.Text;
+            string edad = tbEdad.Text;
+            string genero = "";
+            if (rbFemenino.Checked)
+            {
+                genero = "Femenino";
+            }
+            else if (rbMasculino.Checked)
+            {
+                genero = "Masculino";
+            }
+            string datos = $"Nombre: {nombre}\r\nApellido: {apellido}\r\n" +
+                $"Telefono: {telefono}\r\nEstatura: {estatura}\r\n" +
+                $"Edad: {edad}\r\nGenero: {genero}\r\n";
+            
+            //Guardado del archivo 
+
+            //  string ruta = "C:/Users/HUAWEI/Documents/Semestre 3/Programación avanzada.txt"
+            //  string ruta = @"C:\Users\HUAWEI\Documents\Semestre 3\Programación avanzada";
+            string ruta = "C:\\Users\\HUAWEI\\Documents\\Semestre 3\\3OLIDTS2025.txt";
+            bool archivoExiste = File.Exists(ruta);
+            using (StreamWriter writer = new StreamWriter(ruta, true))
+            {
+                if (archivoExiste)
+                {
+                    writer.WriteLine();
+                }
+                writer.WriteLine(datos);
+            }
+            MessageBox.Show(datos, "Valores ingresados", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
